5700060292b67aa9dbe218030bd4555be5863b34
akanshamalik <akansha.96malik@gmail.com>
2020-08-07 15:37:38 -0500

    newCommit

--- 100-199/100.java
+++ 100-199/100.java
@@ -30,6 +30,8 @@ Output: true
  *     }
  * }
  */
+
+ // Using DFS
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null){
@@ -68,5 +70,22 @@ class Solution {
         }
         return list;
         
+    } 
+}
+
+// Using Recurssion 
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right , q.right);
     } 
 }
