763536a45070c80cd02b4d8c85318dab1cfb20f7
akanshamalik <akansha.96malik@gmail.com>
2020-09-13 17:51:27 -0500

    newCommit

--- 1-99/69.java
+++ 1-99/69.java
@@ -31,3 +31,29 @@ class Solution {
         return (int)ans;
     }
 }
+
+// Second Method
+
+class Solution {
+    public int mySqrt(int x) {
+        long left = 1;
+        long right = x;
+        while(left <= right){
+            long mid = left + (right - left)/2;
+            if(mid * mid == x){
+                // System.out.println("xxxxxxxxxxxxxxxxxxxxxx");
+                return (int)mid;
+            }
+            if(mid*mid > x){
+                right = mid-1;
+            }
+            if(mid* mid < x){
+                left = mid+1 ;
+                // int long = mid;
+            }
+            
+        }
+        
+        return (int)right;
+    }
+}
