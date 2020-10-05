fec341273a9a7e7bca3ab6b6b2943426ad08d9ef
akanshamalik <akansha.96malik@gmail.com>
2020-10-05 16:29:40 -0500

    newCommit

--- 100-199/155.java
+++ 100-199/155.java
@@ -53,3 +53,37 @@ class MinStack {
         return t;
     }
 }
+
+// Using one stack
+class MinStack {
+    
+    Stack<Integer> stack;
+    int min ;
+    /** initialize your data structure here. */
+    public MinStack() {
+        min = Integer.MAX_VALUE;
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if(x <= min){
+            stack.push(min);
+            min =x;
+        }
+        stack.push(x);
+    }
+    public void pop() {
+        int x = stack.pop();
+        if(x == min){
+            min = stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
