edfdaaf2a4302d7a2c6d44cbee13bddc0e46e7a9
akanshamalik <akansha.96malik@gmail.com>
2020-10-05 16:08:01 -0500

    commit

---
+++ 100-199/155.java
@@ -0,0 +1,55 @@
+/**155. Min Stack
+ * 
+ Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+
+Example 1:-
+Input:
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output:
+[null,null,null,null,-3,null,0,-2]
+
+Explanation:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+ */
+
+ //Using arrayList
+class MinStack {
+    
+    List<Integer> list;
+    int y;
+    /** initialize your data structure here. */
+    public MinStack() {
+        list = new ArrayList<>();
+        
+    }
+    
+    public void push(int x) {
+        list.add(x);
+    }
+    public void pop() {
+        list.remove(list.size()-1);
+    }
+    
+    public int top() {
+        return list.get(list.size()-1);
+    }
+    
+    public int getMin() {
+        int t = Collections.min(list);
+        return t;
+    }
+}
