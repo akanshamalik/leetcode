3a75d5fd896b1181bdd19ab3e0594a2dda21d559
akanshamalik <akansha.96malik@gmail.com>
2020-10-15 23:02:48 -0500

    commit

---
+++ 100-199/173.java
@@ -0,0 +1,73 @@
+/**173. Binary Search Tree Iterator
+
+ Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.Calling next() will return the next smallest number in the BST.
+
+Example:
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // return 3
+iterator.next();    // return 7
+iterator.hasNext(); // return true
+iterator.next();    // return 9
+iterator.hasNext(); // return true
+iterator.next();    // return 15
+iterator.hasNext(); // return true
+iterator.next();    // return 20
+iterator.hasNext(); // return false 
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        while(root != null){
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode curr = stack.pop();
+        int x = curr.val;
+        if(curr.right != null){
+            curr = curr.right;
+            stack.push(curr);
+            while(curr.left != null){
+                stack.push(curr.left);
+                curr = curr.left;
+            }
+        }
+        
+        return x;
+        
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+      return !stack.isEmpty();
+    }    
+    
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
