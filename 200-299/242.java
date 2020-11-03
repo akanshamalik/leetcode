e0aee74332d948be2bf637ca13059caf82aea3af
akanshamalik <akansha.96malik@gmail.com>
2020-11-03 13:19:27 -0600

    newCommit

--- 200-299/242.java
+++ 200-299/242.java
@@ -24,3 +24,23 @@ class Solution {
         return true;
     }
 }
+
+//O(n) time complexity
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+       int[] freq = new int[26];
+        for(int i=0; i<s.length();i++){
+            freq[s.charAt(i) -'a']++;
+        }
+        for(int i=0; i<t.length();i++){
+            freq[t.charAt(i) -'a']--;
+            if(freq[t.charAt(i) -'a'] <0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
