b0ea690a9f5ea380017dd6439956f224cea9f361
Akäsh Säiñi <46739435+akash2237778@users.noreply.github.com>
2021-02-24 00:55:14 +0530

    Have a look on this!
    I'm looking for a peer programmer, reply if interested!

--- 1-99/2.java
+++ 1-99/2.java
@@ -28,23 +28,33 @@ Output: [0]
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
-        int carry = 0;
-        while (p != null || q != null) {
-            int x = (p != null) ? p.val : 0;
-            int y = (q != null) ? q.val : 0;
-            int sum = carry + x + y;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (p != null) p = p.next;
-            if (q != null) q = q.next;
+        
+        if(l1 == null && l2 == null){
+            return null;
         }
-        if (carry > 0) {
-            curr.next = new ListNode(carry);
+        if(l1 == null){
+                return new ListNode(l2.val, addTwoNumbers(l1 , l2.next ));
         }
-        return dummyHead.next;
+        if(l2 == null){
+                return new ListNode(l1.val, addTwoNumbers(l1.next, l2));
+        }
+        if(l1.val + l2.val > 9){
+                 int c = (l1.val + l2.val);
+                if(l1.next == null){
+                    l1.next = new ListNode(c/10);
+                    return new ListNode(c%10, addTwoNumbers(l1.next , l2.next)); 
+                }else if(l2.next == null){
+                    l2.next = new ListNode(c/10);
+                    return new ListNode(c%10, addTwoNumbers(l1.next , l2.next)); 
+                }else{
+                    l1.next.val = l1.next.val + c/10;
+                    return new ListNode(c%10, addTwoNumbers(l1.next , l2.next)); 
+                }
+            
+        }else{
+            return new ListNode(l1.val + l2.val, addTwoNumbers(l1.next , l2.next));
+               
+        
+        }
     }
-    
-    }
+}
